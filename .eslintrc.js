92961ca599ec32693354b802b5d6f9508f3156fb
SaaZ <apple@APPLEs-MacBook-Pro.local>
2021-09-24 14:39:38 +0530

    [AZZ-39] code

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
 			'error',
 			'always'
 		],
-		"no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+		"no-unused-vars": "on", // or "@typescript-eslint/no-unused-vars": "off",
 		"unused-imports/no-unused-imports": "error",
 		"unused-imports/no-unused-vars": [
 			"warn",
