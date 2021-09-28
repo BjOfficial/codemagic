814dd41fcd710ca393336983d65f95725b3fb92c
vijaya bharathi <bharathi@transcenddigital.com>
2021-09-28 10:12:17 +0530

    Sonarcloud issue fixed

--- src/screens/InviteFriends/index.js
+++ src/screens/InviteFriends/index.js
@@ -27,8 +27,8 @@ const InviteFriends = () => {
 
 
 
-    const searchClick = (data) => {
-        navigation.navigate(SearchContactNav);
+    const searchClick = (screen) => {
+        navigation.navigate(screen);
     }
     const contactpermission = () => {
         if (Platform.OS === "android") {
@@ -198,7 +198,7 @@ const InviteFriends = () => {
                     
                     <View style={styles.bottomBorder}></View>
                     <Text style={styles.phoneTitle}>Phone Contacts</Text>
-                    <TouchableOpacity onPress={()=>searchClick('data')}><SearchInput disableInput={true} placeholder="search for name,number" value={searchvalue} onChangeText={(data) => { setSearchvalue(data), navigation.navigate(SearchContactNav) }} editable_text={false} backgroundColor={colorsearchbar} icon={search_icon} /></TouchableOpacity>
+                    <TouchableOpacity onPress={()=>searchClick(SearchContactNav)}><SearchInput disableInput={true} placeholder="search for name,number" value={searchvalue} onChangeText={(data) => { setSearchvalue(data), navigation.navigate(SearchContactNav) }} editable_text={false} backgroundColor={colorsearchbar} icon={search_icon} /></TouchableOpacity>
                     <ScrollView 
     scrollEventThrottle={400}>
                     {contactlist&&
