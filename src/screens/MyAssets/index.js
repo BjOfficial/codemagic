7519f8088cb5f600469a75c858114c282bf4b1dd
Divya-17-lakhmi <nithyaprakash@pyramidions.co>
2021-10-07 15:31:05 +0530

    [divyalakshmi] - appliance list

--- src/screens/MyAssets/index.js
+++ src/screens/MyAssets/index.js
@@ -2,20 +2,66 @@ import StatusBar from "@components/StatusBar";
 import ThemedButton from "@components/ThemedButton";
 import { colorLightBlue } from "@constants/Colors";
 import { useNavigation, DrawerActions } from "@react-navigation/native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as RN from "react-native";
-
+import APIKit from "@utils/APIKit";
 import style from "./styles";
 import { AddAssetNav } from "@navigation/NavigationConstant";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { constants } from "@utils/config";
 
 const MyAssets = () => {
   const navigation = useNavigation();
   const navigateToAddAsset = () => {
     navigation.navigate(AddAssetNav);
   };
+  const [applianceList, setApplianceList] = useState([]);
+  useEffect(() => {
+    listDocument();
+  }, []);
+
+  const listDocument = async () => {
+    const getToken = await AsyncStorage.getItem("loginToken");
+    let ApiInstance = await new APIKit().init(getToken);
+    let awaitlocationresp = await ApiInstance.get(constants.listAppliance);
+    if (awaitlocationresp.status == 1) {
+      setApplianceList(awaitlocationresp.data.data);
+    } else {
+      console.log("not listed location type");
+    }
+  };
   const DrawerScreen = () => {
     return navigation.dispatch(DrawerActions.toggleDrawer());
   };
+  const renderItem = ({ item, index }) => {
+    console.log(item);
+    return (
+      <RN.View key={index} style={{ marginTop: 10 }}>
+        <RN.TouchableOpacity>
+          {item.image[0] && item.image ? (
+            <RN.Image
+              source={{
+                uri: "file:///" + item.image[0].path,
+              }}
+              style={{
+                borderWidth: 1,
+                height: RN.Dimensions.get("screen").height / 6,
+                width: RN.Dimensions.get("screen").width / 4,
+                marginLeft: 20,
+                marginRight: 10,
+                borderRadius: 20,
+                paddingLeft: 5,
+              }}
+            />
+          ) : null}
+          <RN.Text style={{ alignSelf: "center" }}>
+            {item.model.name ? item.model.name : item.model.other_value}
+          </RN.Text>
+        </RN.TouchableOpacity>
+      </RN.View>
+    );
+  };
+
   return (
     <RN.View>
       <StatusBar />
@@ -39,23 +85,31 @@ const MyAssets = () => {
           </RN.View>
         </RN.View>
       </RN.View>
-      <RN.View style={style.center}>
-        <RN.Image
-          source={require("../../assets/images/emptyStates/addasset.png")}
-          style={style.image}
+      {applianceList.length > 0 ? (
+        <RN.FlatList
+          data={applianceList}
+          renderItem={renderItem}
+          numColumns={3}
         />
-        <RN.Text style={style.text}>
-          {"Manage your Assets like an Expert"}
-        </RN.Text>
-        <RN.TouchableOpacity onPress={() => navigateToAddAsset()}>
-          <ThemedButton
-            title="+ Add Asset"
-            mode="outline"
-            color={colorLightBlue}
-            buttonStyle={{ marginTop: 20 }}
-            btnStyle={{ fontFamily: "Rubik-Medium" }}></ThemedButton>
-        </RN.TouchableOpacity>
-      </RN.View>
+      ) : (
+        <RN.View style={style.center}>
+          <RN.Image
+            source={require("../../assets/images/emptyStates/addasset.png")}
+            style={style.image}
+          />
+          <RN.Text style={style.text}>
+            {"Manage your Assets like an Expert"}
+          </RN.Text>
+          <RN.TouchableOpacity onPress={() => navigateToAddAsset()}>
+            <ThemedButton
+              title="+ Add Asset"
+              mode="outline"
+              color={colorLightBlue}
+              buttonStyle={{ marginTop: 20 }}
+              btnStyle={{ fontFamily: "Rubik-Medium" }}></ThemedButton>
+          </RN.TouchableOpacity>
+        </RN.View>
+      )}
     </RN.View>
   );
 };
