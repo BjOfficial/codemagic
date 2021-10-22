1b03cf07f72ddd9bf95527ef73e8bfc0bd26aed2
vijaya bharathi <bharathi@transcenddigital.com>
2021-10-22 15:36:09 +0530

    [AZZ-13] Appliaance duplication issue solved, Bottom add tab completed

--- src/screens/Add/index.js
+++ src/screens/Add/index.js
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from "react";
 import * as RN from "react-native";
 import styles from "./style";
 import { my_appliances, document_menu, my_remainders } from "@constants/Images";
-import { useNavigation } from "@react-navigation/native";
-import { AddAssetNav, AddDocumentNav } from "@navigation/NavigationConstant";
-
+import { useNavigation, useIsFocused } from "@react-navigation/native";
+import {
+  AddAssetNav,
+  AddDocumentNav,
+  AddReaminderNav,
+} from "@navigation/NavigationConstant";
+import BottomSheetComp from "@components/BottomSheetComp";
 const Add = () => {
   const [modalVisible, setModalVisible] = useState(true);
   const navigation = useNavigation();
+  const focused = useIsFocused();
+  // if(focused){
+  //   setModalVisible(true);
+  // }
   const [menu] = useState([
     {
       name: "Add Appliances",
@@ -28,14 +36,24 @@ const Add = () => {
       icon: my_remainders,
       height: 20,
       width: 17,
+      route: AddReaminderNav,
     },
   ]);
+
+  const goBack = () => {
+    setModalVisible(false);
+    navigation.goBack();
+  };
   useEffect(() => {
-    setModalVisible(true);
-  }, []);
+    if (focused) {
+      setModalVisible(true);
+    } else {
+      setModalVisible(false);
+    }
+  }, [focused]);
   return (
     <RN.View style={styles.centeredView}>
-      <RN.Modal
+      {/* <RN.Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
@@ -75,7 +93,47 @@ const Add = () => {
             ))}
           </RN.View>
         </RN.View>
-      </RN.Modal>
+      </RN.Modal> */}
+      <BottomSheetComp
+        panelStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+        sheetVisible={modalVisible}
+        closePopup={() => goBack()}>
+        {/* <RN.View style={styles.centeredView}> */}
+        <RN.View style={styles.modalView}>
+          {menu.map((menu, index) => {
+            return (
+              <RN.TouchableOpacity
+                key={index}
+                onPress={() => navigation.navigate(menu.route)}>
+                <RN.View
+                  style={{
+                    flexDirection: "row",
+                    marginTop: 10,
+                    marginLeft: 20,
+                  }}>
+                  <RN.View style={{ flex: 1 }}>
+                    <RN.Image
+                      source={menu.icon}
+                      style={{
+                        height: menu.height,
+                        width: menu.width,
+                        marginTop: menu.marginTop,
+                      }}
+                    />
+                  </RN.View>
+                  <RN.View style={{ flex: 7 }}>
+                    <RN.Text
+                      style={{ fontFamily: "Rubik-Regular", fontSize: 15 }}>
+                      {menu.name}
+                    </RN.Text>
+                  </RN.View>
+                </RN.View>
+              </RN.TouchableOpacity>
+            );
+          })}
+        </RN.View>
+        {/* </RN.View> */}
+      </BottomSheetComp>
     </RN.View>
   );
 };
