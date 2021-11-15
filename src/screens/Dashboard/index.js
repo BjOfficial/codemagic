3b935023e0f63d259d517e0692934046cfb9f791
vijaya bharathi <bharathi@transcenddigital.com>
2021-11-15 20:58:38 +0530

    default image issue checking

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -158,7 +158,6 @@ const Dashboard = () => {
 		listAppliance();
 	}, [isFocused]);
 	const renderItem = ({ item, index }) => {
-		console.log("item",item);
 		try {
 			let categoryName = item.category.name.replace(/ /g, '');
 			let assetName = item.type.name.replace(/ /g, '');
