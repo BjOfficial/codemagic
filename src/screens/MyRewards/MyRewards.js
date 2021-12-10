f0d6e5541426f99f445dbba4343a85af933bde5f
Saipavan Tej <saipavan.tej@pyramidions.co>
2021-12-10 12:01:23 +0530

    [AZZ-485] [Saipavan]

--- src/screens/MyRewards/MyRewards.js
+++ src/screens/MyRewards/MyRewards.js
@@ -1,241 +1,166 @@
 import React from 'react';
-import { View, Text, Image, Dimensions } from 'react-native';
-import HomeHeader from '@components/HomeHeader';
-import { font12 } from '@constants/Fonts';
+import {
+	View,
+	Text,
+	Image,
+	SafeAreaView,
+	TouchableOpacity,
+	StyleSheet,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { colorDropText } from '@constants/Colors';
+import { colorLightBlue, colorWhite } from '@constants/Colors';
+import StatusBar from '@components/StatusBar';
+import { white_arrow } from '@constants/Images';
 // import {reward} from '@constants/Images/reward'
 
-function MyRewards() {
-  return (
-    <View style={{ flex: 1 }}>
-      <HomeHeader title="My Rewards" />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            margin: 20,
-            justifyContent: 'space-between',
-          }}>
-          <Image
-            source={require('@assets/images/rewards/reward.png')}
-            style={{ height: 120, width: 120, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              //   width: "155%",
-              height: Dimensions.get('screen').height / 9,
-              marginLeft: 10,
-              fontWeight: 'bold',
-              color: colorDropText,
-            }}>
-            Earn 25 Coins For Each Invite {'\n'}
-            {'\n'}You Send And 500 Coins If{'\n'}
-            {'\n'} They Install Azzetta.
-          </Text>
-        </View>
-        <Text
-          style={{ marginLeft: 20, fontWeight: 'bold', color: colorDropText }}>
-          How it works:
-        </Text>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks1.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 40,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            Invite your friends to use Azzetta App and earn 25 Azzeti coins
-            for each invitation sent by you.
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks2.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 40,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            After your friend downloads and installs Azzetta App within 60
-            days you earn 500 Azzeti coins.
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks3.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 55,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            Your direct contacts who are already using Azzetta App are
-            automatically added to your network as first circle (FC).
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks4.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 70,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            When your FC users invite their friends and those who install
-            Azzetta App within 60 days of receiving their invitation
-            become your second circle (SC) of trusted network.
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks5.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 40,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            For every second circle users you get 20 more Azzeti coins as
-            your reward.
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks6.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              //   width: "100%",
-              height: 70,
-              marginLeft: 10,
-              color: colorDropText,
-              lineHeight: 18
-            }}>
-            You need to subscribe to join Azzetta Club with 200 Azzeti
-            coins and renew membership subscription at 200 Azzeti coins
-            per year.
-          </Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 20
-          }}>
-          <Image
-            source={require('@assets/images/rewards/howitworks7.png')}
-            style={{ height: 15, width: 20, resizeMode: 'contain' }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              fontSize: font12,
-              // width: "100%",
-              height: 40,
-              marginLeft: 10,
-              color: colorDropText,
-            }}>
-            Each year 10% of your Azzetta coins will be donated to our
-            charity partners.
-          </Text>
-        </View>
-      </ScrollView>
-    </View>
-  );
+function MyRewards(props) {
+	const data = [
+		{
+			icon: require('@assets/images/rewards/howitworks1.png'),
+			text: 'Invite your friends to use Azzetta App and earn 25 Azzeti coins for each invitation sent by you.',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks2.png'),
+			text: 'After your friend downloads and installs Azzetta App within 60 days you earn 500 Azzeti coins.',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks3.png'),
+			text: 'Your direct contacts who are already using Azzetta App are automatically added to your network as first circle (FC).',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks4.png'),
+			text: 'When your FC users invite their friends and those who install Azzetta App within 60 days of receiving their invitation become your second circle (SC) of trusted network.',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks5.png'),
+			text: 'For every second circle users you get 20 more Azzeti coins as your reward.',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks6.png'),
+			text: 'You need to subscribe to join Azzetta Club with 200 Azzeti coins and renew membership subscription at 200 Azzeti coins per year.',
+		},
+		{
+			icon: require('@assets/images/rewards/howitworks7.png'),
+			text: 'Each year 10% of your Azzetta coins will be donated to our charity partners.',
+		},
+	];
+	const banner = () => {
+		return (
+			<View
+				style={{
+					flexDirection: 'row',
+					marginVertical: 10,
+					marginHorizontal: 20,
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}>
+				<View style={{ height: 120, width: 120 }}>
+					<Image
+						style={{ height: '100%', width: '100%', resizeMode: 'contain' }}
+						source={require('@assets/images/rewards/reward.png')}
+					/>
+				</View>
+				<View
+					style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<Text
+						style={{
+							fontFamily: 'Rubik-Medium',
+							color: '#393939',
+							margin: 20,
+							fontSize: 13,
+						}}>
+						Earn 25 Coins For Each Invite You Send And 500 Coins If They Install
+						Azzetta.
+					</Text>
+				</View>
+			</View>
+		);
+	};
+	const points = (data) => {
+		return (
+			<View
+				style={{
+					flexDirection: 'row',
+					marginVertical: 10,
+					marginHorizontal: 20,
+					flex: 1,
+				}}>
+				<View style={{ height: 20, width: 20 }}>
+					<Image
+						style={{ height: '100%', width: '100%', resizeMode: 'contain' }}
+						source={data.icon}
+					/>
+				</View>
+				<View style={{ flex: 1, paddingLeft: 15 }}>
+					<Text style={{ fontFamily: 'Rubik-Regular', color: '#393939' }}>
+						{data.text}
+					</Text>
+				</View>
+			</View>
+		);
+	};
+	return (
+		<View style={{ flex: 1, backgroundColor: colorWhite }}>
+			<SafeAreaView style={{ backgroundColor: colorLightBlue }} />
+			<StatusBar />
+			<View style={style.navbar}>
+				<View style={style.navbarRow}>
+					<TouchableOpacity
+						onPress={() => {
+							props.navigation.goBack();
+						}}>
+						<View>
+							<Image source={white_arrow} style={style.notificationIcon} />
+						</View>
+					</TouchableOpacity>
+					<View>
+						<Text style={style.navbarName}>{'My Rewards'}</Text>
+					</View>
+				</View>
+			</View>
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{paddingVertical:10}}>
+				{banner()}
+				<Text
+					style={{
+						fontFamily: 'Rubik-Medium',
+						color: '#393939',
+						margin: 20,
+						fontSize: 16,
+					}}>
+					{'How it works :'}
+				</Text>
+				{data.map((item) => {
+					return points(item);
+				})}
+			</ScrollView>
+		</View>
+	);
 }
+
+const style = StyleSheet.create({
+	navbarRow: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	notificationIcon: {
+		height: 15,
+		width: 20,
+		margin: 20,
+	},
+	navbar: {
+		backgroundColor: colorLightBlue,
+		borderBottomLeftRadius: 30,
+		borderBottomRightRadius: 30,
+	},
+	navbarName: {
+		color: colorWhite,
+		fontFamily: 'Rubik-Medium',
+		fontSize: 17,
+		marginVertical: 15,
+	},
+});
 
 export default MyRewards;
