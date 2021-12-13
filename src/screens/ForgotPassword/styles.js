1cb62fd68cd1f0aa23035a0fa0fd3e76671403ae
Saipavan Tej <saipavan.tej@pyramidions.co>
2021-12-13 11:36:50 +0530

    [es-lint] [Saipavan]

--- src/screens/ForgotPassword/styles.js
+++ src/screens/ForgotPassword/styles.js
@@ -1,41 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet} from 'react-native';
 import {
-	colorWhite,
-	colorplaceholder,
-	colorSuccess,
-	colorBlack,
+  colorWhite,
+  colorplaceholder,
+  colorSuccess,
+  colorBlack,
 } from '@constants/Colors';
 import { font14, font20 } from '@constants/Fonts';
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: colorWhite,
-	},
-	headerText: {
-		fontSize: font20,
-		fontFamily: 'Rubik-Medium',
-		paddingTop: 25,
-		color: colorBlack,
-	},
-	Invitepara: {
-		fontSize: font14,
-		fontFamily: 'Rubik-Regular',
-		color: colorplaceholder,
-		marginBottom: 20,
-		paddingVertical: 10,
-		lineHeight: 24,
-	},
-	errMsg: {
-		fontSize: 12,
-		color: 'red',
-		fontFamily: 'Avenir-Roman',
-		textAlign: 'center',
-	},
-	successMsg: {
-		color: colorSuccess,
-		fontSize: 12,
-		textAlign: 'center',
-		fontFamily: 'Avenir-Roman',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: colorWhite,
+  },
+  headerText: {
+    fontSize: font20,
+    fontFamily: 'Rubik-Medium',
+    paddingTop: 25,
+    color: colorBlack,
+  },
+  Invitepara: {
+    fontSize: font14,
+    fontFamily: 'Rubik-Regular',
+    color: colorplaceholder,
+    marginBottom: 20,
+    paddingVertical: 10,
+    lineHeight: 24,
+  },
+  errMsg: {
+    fontSize: 12,
+    color: 'red',
+    fontFamily: 'Avenir-Roman',
+    textAlign: 'center',
+  },
+  successMsg: {
+    color: colorSuccess,
+    fontSize: 12,
+    textAlign: 'center',
+    fontFamily: 'Avenir-Roman',
+  },
 });
 export default styles;
