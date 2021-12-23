75f2f10c6e962aea738b9491373f40f418ace86a
Saipavan Tej <saipavan.tej@pyramidions.co>
2021-12-23 15:17:59 +0530

    [AZZ-847] [Saipavan]

--- src/screens/Documents/index.js
+++ src/screens/Documents/index.js
@@ -18,8 +18,9 @@ import {
   AddDocumentNav,
   DocumentViewNav,
 } from '@navigation/NavigationConstant';
+import moment from 'moment';
 import { constants } from '@utils/config';
-import { documentDefaultImages, noDocument } from '@constants/Images';
+import { documentDefaultImages, noDocument, alertclock, defImgeView } from '@constants/Images';
 import Loader from '@components/Loader';
 import PdfThumbnail from 'react-native-pdf-thumbnail';
 
@@ -37,6 +38,9 @@ const Documents = () => {
 	const ref = React.useRef(null);
 	const [fullLoder, setFullLoder] = useState(true);
 	const [pdfDocumentView, setPdfDocumentView] = useState(false);
+	const [defImgeView, setDefImgeView] = useState();
+	const [documentAlert, setDocumentAlert] = useState([]);
+	const [documentDefaultImageView, setDocumentDefImgeView] = useState(false);
 	useScrollToTop(ref);
 
 	useEffect(() => {
@@ -47,11 +51,15 @@ const Documents = () => {
 			settotalrecords(0);
 			setupdatedCount(0);
 			listDocument(1);
+			setDocumentAlert([]);
+			getDocumentAlert();
 		});
 	}, []);
 	useEffect(()=>{
 		if(isFocused){
 		listDocument(1);
+		setDocumentAlert([]);
+		getDocumentAlert();
 		}
 		// if(networkStatus==true){
 		// 	setFullLoder(false);
@@ -64,6 +72,15 @@ const Documents = () => {
     documentListTemp[index].fileDataDoc = false;
     setDocumentList([...documentListTemp]);
   };
+
+  const notifyMessage = (msg) => {
+    if (RN.Platform.OS === 'android') {
+      RN.ToastAndroid.show(msg, RN.ToastAndroid.SHORT);
+    } else {
+      RN.Alert.alert(msg);
+    }
+  };
+
   const documentResultHandling =(records)=>{
 	records.forEach((list) => {
 		var defImg;
@@ -87,6 +104,26 @@ const Documents = () => {
 	});
 	return records;
   }
+
+  const getDocumentAlert = async () => {
+    const getToken = await AsyncStorage.getItem('loginToken');
+    let ApiInstance = await new APIKit().init(getToken);
+    let getDocumentAlertresp = await ApiInstance.get(constants.listDocumentAlert);
+    if (getDocumentAlertresp.status == 1) {
+      setDocumentAlert(getDocumentAlertresp.data.data);
+      try {
+        let documentName = getDocumentAlertresp.data.data[0].document_type.name.replace(/ /g, '').toLowerCase();
+        let categoryName = 'Others';
+        documentDefaultImages.forEach((documentType) => {
+          setDefImgeView(documentType[documentName][categoryName].url);
+        });
+      } catch (e) {
+        setDefImgeView(noDocument);
+      }
+    } else {
+      notifyMessage(JSON.stringify(getDocumentAlertresp));
+    }
+  };
 
 	const listDocument = async (data) => {
 		const getToken = await AsyncStorage.getItem('loginToken');
@@ -155,7 +192,7 @@ const Documents = () => {
 				}>
 				<RN.View
 					style={{
-						marginHorizontal: 15,
+						marginHorizontal: 10,
 						marginVertical:15,
 						elevation: 12,
 						shadowColor: '#000',
@@ -173,25 +210,12 @@ const Documents = () => {
 						// height: RN.Dimensions.get('screen').height / 8,
 						// width: RN.Dimensions.get('screen').width / 4,
 					}}>
-{!pdfDocumentView ?
 					<RN.Image
 						source={{
 							uri: item.fileDataDoc
 								? item.setImage
 								: RN.Image.resolveAssetSource(item.defaultImage).uri,
 						}}
-						onError={(e) => setPdfDocumentView(true)}
-						style={{
-							height: '100%',
-							width: '100%',
-							borderRadius: 10,
-							resizeMode: item.fileDataDoc?'cover':'contain',
-						}}
-					/>
-					:<RN.Image
-						source={
-								item.setImage
-						}
 						onError={(e) => onDocumentImageLoadingError(e, index)}
 						style={{
 							height: '100%',
@@ -199,7 +223,7 @@ const Documents = () => {
 							borderRadius: 10,
 							resizeMode: item.fileDataDoc?'cover':'contain',
 						}}
-					/>}
+					/>
 				</RN.View>
 				<RN.View
 					style={{
@@ -273,6 +297,7 @@ const Documents = () => {
         </RN.View>
       </RN.View>
       <RN.ScrollView
+	    showsVerticalScrollIndicator={false}
         ref={ref}
         onScroll={({ nativeEvent }) => {
           if (isCloseToBottom(nativeEvent)) {
@@ -289,6 +314,43 @@ const Documents = () => {
           }
         }}
         scrollEventThrottle={400}>
+			{documentAlert.length > 0 &&
+                  <RN.TouchableOpacity onPress={() => { navigation.navigate('DocumentRemainder', {
+                    from: 'myReminders',
+                    document_ids: documentAlert[0]._id,
+                    reminder_data: 'editDocumentReminder',
+                    comments:  documentAlert[0].reminder.comments,
+                    title: documentAlert[0].reminder.title._id,
+                    date: documentAlert[0].reminder.date,
+                    otherTitle: documentAlert[0].reminder.title.other_value,
+                  });}} 
+                  style={{ marginHorizontal: 10, marginBottom: 10, marginTop: 10,backgroundColor: '#EDF0F7', flexDirection: 'row', padding: 10, borderRadius: 10}}>
+                    <RN.View style={{ height: 100, width: 100, borderRadius: 10, paddingRight: 10}}>
+                      <RN.Image
+                        source={
+							documentAlert && documentAlert.image && !documentDefaultImageView ? {
+							  uri: documentAlert[0].image[0]
+							} :
+							  defImgeView}
+                        style={{ height: '100%', width: '100%', resizeMode: 'cover', borderRadius: 10 }} />
+                    </RN.View>
+                    <RN.View style={{flex:1, alignSelf: 'center'}}>
+                      <RN.View style={{flexDirection:'row', justifyContent:'space-between', flex:1}}>
+                        <RN.View style={{alignSelf:'center',flex:1,paddingRight:10}}>
+                          <RN.Text style={{ color: '#393939', fontFamily: 'Rubik-Medium', fontSize: 12}}>{documentAlert[0]?.document_type.name == 'Others'? documentAlert[0]?.document_type.other_value : documentAlert[0]?.document_type.name}</RN.Text>
+                        </RN.View>
+                        <RN.View style={{ height: 30, width: 30 }}>
+                          <RN.Image source={alertclock} style={{ height: '100%', width: '100%', resizeMode: 'cover'}} />
+                        </RN.View>
+                      </RN.View>
+                      <RN.View style={{ backgroundColor: '#6BB3B3', paddingHorizontal: 10, borderRadius: 8, marginRight:15,flex:1, justifyContent:'center'}}>
+                        <RN.Text style={{ color: '#FFFFFF', fontFamily: 'Rubik-Regular', fontSize: 12 }}>{documentAlert[0].reminder.title.name == 'Others'
+                          ? documentAlert[0].reminder.title.other_value + ` on ${moment(new Date(documentAlert[0]?.reminder.date)).format('DD/MM/YYYY')}`
+                          : documentAlert[0].reminder.title.name + ` on ${moment(new Date(documentAlert[0]?.reminder.date)).format('DD/MM/YYYY')}`}</RN.Text>
+                      </RN.View>
+                    </RN.View>
+                  </RN.TouchableOpacity>
+                }
         {totalrecords > 0 ? (
           <RN.FlatList
             data={documentList}
